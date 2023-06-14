12738db695cc84c48627b9f9b41a832fdf42466d
ADAMU MUHAMMAD MUHAMMAD <adamsgeeky@localhost.localdomain>
2023-06-14 15:19:44 +0100

    geekinkCLI

--- Cli/geekink-CLI/index.mjs
+++ Cli/geekink-CLI/index.mjs
@@ -1,32 +1,37 @@
 #!/usr/bin/env node
 import fetch from 'node-fetch';
 
+// colors for console
 const colors = {
-    red: '\x1b[31m',
-    green: '\x1b[32m',
-    yellow: '\x1b[33m'
-  };
+  red: '\x1b[31m',
+  green: '\x1b[32m',
+  yellow: '\x1b[33m'
+};
 
+// get arguments from command line
 const args = process.argv.slice(2);
 const usernameIndex = args.findIndex(arg => arg === '-u' || arg === '--username');
 const repositoryIndex = args.findIndex(arg => arg === '-r' || arg === '--repository');
 
+// check if arguments are valid
 if (usernameIndex === -1 && repositoryIndex === -1 && usernameIndex + 1 >= args.length && repositoryIndex + 1 >= args.length) {
-    console.error(colors.red + 'Usage: geekink -u <username> -r <repository>');
-    process.exit(1);
-}else if(usernameIndex === -1 || usernameIndex + 1 >= args.length){
-    console.error(colors.yellow + 'Usage: geekink -u <username>');
-    process.exit(1);
-}else if(repositoryIndex === -1 || repositoryIndex + 1 >= args.length){
-    console.error(colors.yellow + 'Usage: geekink -r <repository>');
-    process.exit(1);
+  console.error(colors.red + 'Usage: geekink -u <username> -r <repository>');
+  process.exit(1);
+} else if (usernameIndex === -1 || usernameIndex + 1 >= args.length) {
+  console.error(colors.yellow + 'Usage: geekink -u <username>');
+  process.exit(1);
+} else if (repositoryIndex === -1 || repositoryIndex + 1 >= args.length) {
+  console.error(colors.yellow + 'Usage: geekink -r <repository>');
+  process.exit(1);
 }
-
+// get username and repository from arguments
 const username = args[usernameIndex + 1];
 const repository = args[repositoryIndex + 1];
 
+// create url for github api
 const url = `https://api.github.com/repos/${username}/${repository}/issues`;
 
+// fetch data from github api 
 fetch(url)
   .then(response => response.json())
   .then(data => {
